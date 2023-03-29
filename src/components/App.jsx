da9610109d80f76fc236bd90078d11aa2f2c5006
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-03-29 10:01:30 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -16,7 +16,6 @@ import { Home } from 'pages/Home';
 
 export function App() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
